799de4a7fa66d85ef50107b145ef808746980c55
1398404039@qq.com <LwZ&19930516>
2018-10-11 14:34:54 +0800

    添加分支回调接口

--- src/main/java/com/lwz/pay_sys/controller/live/LiveController.java
+++ src/main/java/com/lwz/pay_sys/controller/live/LiveController.java
@@ -1,4 +1,9 @@
 package com.lwz.pay_sys.controller.live;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/live")
 public class LiveController {
 }
