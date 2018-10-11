0713f01e0cb68a531b3cbb6bec6113bd7a29c8cc
1398404039@qq.com <LwZ&19930516>
2018-10-11 18:12:47 +0800

    添加微信支付相关

---
+++ src/main/java/com/lwz/pay_sys/utils/aliUtils/AlipayConfig.java
@@ -0,0 +1,35 @@
+package com.lwz.pay_sys.utils.aliUtils;
+
+import com.lwz.pay_sys.constant.Constants;
+
+/* *
+ *类名：AlipayConfig
+ *功能：基础配置类
+ *详细：设置帐户有关信息及返回路径。
+ */
+public class AlipayConfig {
+
+    // 应用ID,您的APPID，收款账号既是您的APPID对应支付宝账号
+    public static String app_id = "******";
+    // 商户私钥，您的PKCS8格式RSA2私钥
+    public static String merchant_private_key = "******";
+    public static String alipay_public_key = "******";
+
+    // 签名方式
+    public static String sign_type = "RSA2";
+
+    // 字符编码格式
+    public static String charset = "utf-8";
+
+    // 支付宝网关
+    public static String gatewayUrl = "https://openapi.alipay.com/gateway.do";
+    //格式
+    public static String format = "JSON";
+
+    //pc端
+    public static String toSucessPage = Constants.LOCALHOST + "/jiaofeiSuccess.html";
+    //回调
+    public static String paySuccessCall = Constants.LOCALHOST + "/pay/alipay/paySuccessCall";
+
+}
+
---
+++ src/main/java/com/lwz/pay_sys/utils/wxUtils/PayConfigUtil.java
@@ -0,0 +1,23 @@
+package com.lwz.pay_sys.utils.wxUtils;
+
+import com.lwz.pay_sys.constant.Constants;
+
+/**
+ * @Author:lwz
+ * @Description:
+ * @Date:2018/1/6 22:30
+ */
+public class PayConfigUtil {
+    // 账号信息
+    public static final String appid = "******";  // appid 填写自己的
+    public static final String MCH_ID = "******"; // 商业号 填写自己微信的
+    public static final String API_KEY = "******"; // Api key 填写自己微信的
+    public static final String APP_SECRET = ""; // appsecret 填写自己的
+    public static final String NOTIFY_URL = Constants.LOCALHOST + "/pay/wxPay/Notify"; //回调url
+    public static final String CREATE_IP = "112.74.135.224";  //ip地址
+    public static final String UFDODER_URL = "https://api.mch.weixin.qq.com/pay/unifiedorder"; //统一下单url，微信提供的
+    public static final String QUERY_ORDER_STATE = "https://api.mch.weixin.qq.com/pay/orderquery"; //查询订单支付状态url，微信提供
+    public static final String TRADE_TYPE = "NATIVE";
+    public static final String DEVICE_INFO = "H5";
+
+}
