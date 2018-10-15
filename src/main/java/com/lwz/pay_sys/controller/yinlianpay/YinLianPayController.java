2894a02250bddbd11d347a1656329859dfbc80c4
1398404039@qq.com <LwZ&19930516>
2018-10-15 17:25:34 +0800

    添加银联支付

---
+++ src/main/java/com/lwz/pay_sys/controller/yinlianpay/YinLianPayController.java
@@ -0,0 +1,12 @@
+package com.lwz.pay_sys.controller.yinlianpay;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/ylPay")
+public class YinLianPayController {
+    private static Logger logger = LoggerFactory.getLogger(YinLianPayController.class);
+}
